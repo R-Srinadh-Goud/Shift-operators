e89436bbf863767093891c03ffe6b45ab694a222
R-Srinadh-Goud <java19012025@gmail.com>
2025-01-31 00:05:06 +0530

    Add files via upload

---
+++ ShiftOperatorsEg.java
@@ -0,0 +1,14 @@
+public class ShiftOperatorsEg{
+    public static void main(String[] args) {
+        int num = 5;   // Binary: 101
+        int result_L= num << 2;  // Left shift by 2 (multiply by 4)
+        System.out.println("Left Shift: " + result_L);  // Output: 20
+        
+                int result_R = num >> 2;  // Right shift by 2 (divide by 4)
+                System.out.println("Right Shift: " + result_R);  // Output: 5
+            }
+        }
+  
+    
+
+
